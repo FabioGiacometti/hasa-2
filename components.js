9a9868f01592c01b208e0c8c7725ff530acd657e
fabio.giacometti <fabiog.inbox@gmail.com>
2021-04-23 19:44:31 -0300

    first commit

---
+++ components.js
@@ -0,0 +1,19 @@
+import {  hero,
+    clients,
+    products,
+    obras  } from "./main.js"
+
+function imageComponent(dataset) {
+//   for (let i = 0; i < dataset.length; i++) {
+//     // var img = document.createElement("img");
+//     // img.src = `dataset.url`;
+//     // var src = document.getElementById("header");
+//     // src.appendChild(img);}
+//     console.log(dataset);
+//   
+console.log(dataset)
+}
+
+imageComponent(clients)
+
+export { imageComponent }
---
+++ main.js
@@ -0,0 +1,225 @@
+const hero = [
+  { url: "./assets/imgs/hero_/slider-1.jpg" },
+  { url: "./assets/imgs/hero_/slider-2.jpg" },
+  { url: "./assets/imgs/hero_/slider-3.jpg" },
+  { url: "./assets/imgs/hero_/slider-5.jpg" },
+];
+
+const clients = [
+  {
+    url: "http://www.adc.net.ar/",
+    name: "Aguas del Colorado",
+    image: "./assets/imgs/clients/logo-aguas-colorado.png",
+  },
+  {
+    url: "https://www.aguascordobesas.com.ar/",
+    name: "Aguas Cordobesas",
+    image: "./assets/imgs/clients/logo-aguas.jpg",
+  },
+  {
+    url: "https://cosagltda.com/",
+    name: "Coop. trab. de obras sanitarias Alta Gracia Limitada",
+    image: "./assets/imgs/clients/logo-alta-gracia.jpg",
+  },
+  {
+    url: "https://www.exportadoresdecordoba.com/empresas/arc-srl-2810.html",
+    name: "ARC Empresa Constructora",
+    image: "./assets/imgs/clients/logo-arc-1.png",
+  },
+  {
+    url: "http://www.coopi.com.ar/",
+    name: "Cooperativa Crecer Carlos Paz",
+    image: "./assets/imgs/clients/logo-carlos-paz.png",
+  },
+  {
+    url: "https://www.cclipsa.com.ar/",
+    name: "CClip",
+    image: "./assets/imgs/clients/logo-cclip.jpg",
+  },
+  {
+    url: "http://www.conorvial.com.ar/",
+    name: "Conorvial",
+    image: "./assets/imgs/clients/logo-conorvial.jpg",
+  },
+  {
+    url:
+      "https://www.essapp.coop/cooperativa-de-viviendas-agua-potable-y-servicios-publicos-servicios-asistenciales-y-turismo-cosquin",
+    name: "Cooperativa de servicios Cosquin",
+    image: "./assets/imgs/clients/logo-coop-cosquin.png",
+  },
+  {
+    url: "",
+    name: "Cooperativa de servicios Cruz del Eje",
+    image: "./assets/imgs/clients/logo-coop-cruz.png",
+  },
+  {
+    url: "http://www.cooprc.com.ar/",
+    name: "Cooperativa de obras y servicios Rio Ceballos",
+    image: "./assets/imgs/clients/logo-coop-rio-ceballos.jpg",
+  },
+  {
+    url: "https://estructuras.com.ar/",
+    name: "Estructuras S.A.",
+    image: "./assets/imgs/clients/logo-estructuras.png",
+  },
+  {
+    url: "http://www.gieco.com.ar/",
+    name: "Gieco",
+    image: "./assets/imgs/clients/logo-gieco.png",
+  },
+  {
+    url: "http://henisa.com/",
+    name: "Henisa Sudamericana S.A.",
+    image: "./assets/imgs/clients/logo-henisa.png",
+  },
+  {
+    url:
+      "https://www.cba.gov.ar/concursos-publicos/concursos-tramo-personal-superior/ministerio-de-infraestructura/",
+    name: "Ministerio de Infraestructura de la provincia de Córdoba",
+    image: "./assets/imgs/clients/logo-infra.png",
+  },
+  {
+    url: "./assets/imgs/logos/logo-install.png",
+    name: "Inst'all Srl",
+    image: "./assets/imgs/clients/logo-install.png",
+  },
+  {
+    url: "https://item.com.ar/",
+    name: "Item Construcciones S.A.",
+    image: "./assets/imgs/clients/logo-item.jpg",
+  },
+  {
+    url: "http://www.marinellisa.com/",
+    name: "Marinelli",
+    image: "./assets/imgs/clients/logo-marinelli.jpg",
+  },
+  {
+    url: "http://www.michelotti.com.ar/",
+    name: "Michelotti",
+    image: "./assets/imgs/clients/logo-michelotti.jpg",
+  },
+  {
+    url:
+      "https://www.cba.gov.ar/ministerio-de-agua-ambiente-y-servicios-publicos-3/",
+    name: "Ministerio de Agua, Ambiente y Servicios Públicos Cordoba",
+    image: "./assets/imgs/clients/logo-min-agua.png",
+  },
+  {
+    url: "https://www.facebook.com/redexconstruccionessrlcordoba/",
+    name: "Redex",
+    image: "./assets/imgs/clients/logo-redex.jpg",
+  },
+  {
+    url: "http://www.roggio.com.ar/",
+    name: "Roggio",
+    image: "./assets/imgs/clients/logo-roggio.png",
+  },
+  {
+    url: "http://romerocammisa.com.ar/",
+    name: "Romero Camisa Construcciónes S.A.",
+    image: "./assets/imgs/clients/logo-romero.png",
+  },
+  {
+    url: "http://www.rovellacarranza.com.ar/",
+    name: "Rovella Construcciónes",
+    image: "./assets/imgs/clients/logo-rovella.png",
+  },
+];
+
+const products = [
+  { image: "./assets/imgs/products/product_(1).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(2).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(3).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(4).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(5).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(6).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(7).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(8).jpg", name: "Producto" },
+  { image: "./assets/imgs/products/product_(9).jpeg", name: "Producto" },
+];
+
+const obras = [
+  {
+    active: true,
+    title: "Alberdi",
+    date: "Enero 2020 - Enero 2021",
+    contract: "Proveedor - Contratista",
+    images: [
+      "./assets/imgs/obras/alberdi(1).jpg",
+      "./assets/imgs/obras/alberdi(2).jpg",
+      "./assets/imgs/obras/alberdi(3).jpg",
+      "./assets/imgs/obras/alberdi(4).jpg",
+    ],
+  },
+  {
+    title: "Los Cedros",
+    date: "Enero 2020 - Enero 2021",
+    contract: "Proveedor - Contratista",
+    images: [
+      "./assets/imgs/obras/cedros(1).jpg",
+      "./assets/imgs/obras/cedros(2).jpg",
+      "./assets/imgs/obras/cedros(3).jpg",
+    ],
+  },
+  {
+    title: "Plaza España",
+    date: "Enero 2020 - Enero 2021",
+    contract: "Proveedor - Contratista",
+    images: [
+      "./assets/imgs/obras/plaza_espana(1).jpg",
+      "./assets/imgs/obras/plaza_espana(2).jpg",
+      "./assets/imgs/obras/plaza_espana(3).jpg",
+    ],
+  },
+  {
+    title: "Sin Nombre",
+    date: "Enero 2020 - Enero 2021",
+    contract: "Proveedor - Contratista",
+    images: [
+      "./assets/imgs/obras/sin_nombre(1).jpg",
+      "./assets/imgs/obras/sin_nombre(2).jpg",
+      "./assets/imgs/obras/sin_nombre(3).jpg",
+    ],
+  },
+  {
+    title: "Suquia",
+    date: "Enero 2020 - Enero 2021",
+    contract: "Proveedor - Contratista",
+    images: [
+      "./assets/imgs/obras/suquia(1).jpg",
+      "./assets/imgs/obras/suquia(2).jpg",
+      "./assets/imgs/obras/suquia(3).jpg",
+      "./assets/imgs/obras/suquia(4).jpg",
+      "./assets/imgs/obras/suquia(5).jpg",
+      "./assets/imgs/obras/suquia(6).jpg",
+    ],
+  },
+  {
+    title: "Tropezon",
+    date: "Enero 2020 - Enero 2021",
+    contract: "Proveedor - Contratista",
+    images: [
+      "./assets/imgs/obras/tropezon(1).jpg",
+      "./assets/imgs/obras/tropezon(2).jpg",
+      "./assets/imgs/obras/tropezon(3).jpg",
+      "./assets/imgs/obras/tropezon(4).jpg",
+    ],
+  },
+];
+
+
+let simpleSlide = (data) => `<div class="swiper-slide"><img src='${data}'></div>`
+
+const slideGroup = (dataset, component) => {
+    let arr = []
+    for (let i = 0; i < dataset.length; i++) {
+        arr.push(component(dataset[i].image))
+    }
+    return arr.join("");}
+
+let section = (id) => document.getElementById(`${id}-swiper`)
+let carouselInserter = (section, component) => section.innerHTML = slideGroup(section, component)
+
+carouselInserter("clients", simpleSlide)
+
+
